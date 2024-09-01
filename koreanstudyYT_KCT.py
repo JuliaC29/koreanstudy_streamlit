3be67671d01615c231ae7917fdfd3e370a6d7128
Juyeon Chung <chungjulia29@gmail.com>
2024-09-01 19:19:16 -0400

    channel & maxresults & slicing controls updated

--- koreanstudyYT_KCT.py
+++ koreanstudyYT_KCT.py
@@ -146,7 +146,7 @@ def search_videos(query):
     # Sort all videos by view count
     all_videos.sort(key=lambda x: int(get_video_details(x['id']['videoId'])['viewCount']), reverse=True)
     
-    return all_videos[:50]  # Return top 50 most viewed videos across all channels
+    return all_videos[:10]  # Return top 10 most viewed videos across all channels
 
 # Function to get video details such as view count
 @st.cache_data(ttl=86400)
