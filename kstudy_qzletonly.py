da9ec3506fe0690c592f1118eabcba65060afb5d
Juyeon Chung <chungjulia29@gmail.com>
2024-09-08 21:55:17 -0400

    Initial commit

---
+++ kstudy_qzletonly.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import pandas as pd
+
+
+
+# Set up logging
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# Custom CSS to center the title
+st.markdown("""
+<style>
+    .title {
+        text-align: center;
+    }
+</style>
+""", unsafe_allow_html=True)
+
+# Load CSV data
+@st.cache_data
+def load_csv_data():
+    try:
+        df = pd.read_csv('data/fcstr1.csv')
+        lesson_list = df['lesson'].unique().tolist()      
+        return df, lesson_list
+    except Exception as e:
+        st.error(f"Error loading CSV file: {e}")
+        return None, []
+
+df, lesson_list = load_csv_data()
+
+# Function to retrieve Quizlet links for selected lessons
+def get_lesson_link(lesson):
+    try:
+        row = df[df['lesson'] == lesson].iloc[0]
+        link = row['link']
+        lesson_code = row['lesson_code']
+        logger.info(f"Successfully retrieved link for lesson: {lesson}")
+        return link, lesson_code
+    except Exception as e:
+        logger.error(f"Error in get_lesson_link: {e}")
+        return None, None
+
+# Streamlit app setup with tabs for different sections
+st.markdown("<h1 class='title'>한국어 단어 공부</h1>", unsafe_allow_html=True)
+tab1= st.tabs(["Vocabulary"])
+
+with tab1[0]:
+    lesson = st.selectbox("Select a lesson", lesson_list)
+    if lesson:
+        link, lesson_code = get_lesson_link(lesson)
+        if link and lesson_code:
+            st.markdown("Click: " f"[{lesson_code}]({link})", unsafe_allow_html=True)
+  
+
+
