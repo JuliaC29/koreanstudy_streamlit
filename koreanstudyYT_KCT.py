6ac8adfed8c1489952a39cf2eb5bf0b13fbf55c0
Juyeon Chung <chungjulia29@gmail.com>
2024-09-14 12:56:19 -0400

    scope updated

--- koreanstudyYT_KCT.py
+++ koreanstudyYT_KCT.py
@@ -269,12 +269,8 @@ with tab3:
 with tab4:
 
     # Define the correct Google Sheets scopes
-    SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets", 
-    "https://www.googleapis.com/auth/drive.file", 
-    "https://www.googleapis.com/auth/drive"
-    ]
-
+    SCOPE = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+            "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
 
     # Load Google service account credentials from Streamlit secrets
