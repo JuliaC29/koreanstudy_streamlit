be27a414622e512f3523ad74c59569e7bc8a2074
Juyeon Chung <chungjulia29@gmail.com>
2025-01-08 23:27:39 -0500

    channel added

--- koreanstudyYT_KCT.py
+++ koreanstudyYT_KCT.py
@@ -472,7 +472,8 @@ with tab3:
         channel_options = {
             "youquizontheblock": "UC920m3pMPH45qztdhppZhwA",
             "SBS Running Man": "UCaKod3X1Tn4c7Ci0iUKcvzQ",
-            "DdeunDdeun": "UCDNvRZRgvkBTUkQzFoT_8rA",  
+            "DdeunDdeun": "UCDNvRZRgvkBTUkQzFoT_8rA", 
+            "channel fullmoon" : "UCQ2O-iftmnlfrBuNsUUTofQ" 
     
         }
         selected_channel = st.selectbox("Select Channel", options=list(channel_options.keys()))
